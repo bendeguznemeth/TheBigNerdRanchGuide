18d9b80038c1c41d22ee98b3f5a74a50edc268ac
Bendegúz Németh <n.bendeguz3@gmail.com>
2018-12-01 13:40:33 +0100

    Initial Commit

---
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Németh Bendegúz on 2018. 12. 01..
+//  Copyright © 2018. Németh Bendegúz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
