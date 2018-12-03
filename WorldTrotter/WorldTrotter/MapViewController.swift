01b3b22b501fb8c73ec0e302bee196b471b7a508
Bendegúz Németh <n.bendeguz3@gmail.com>
2018-12-03 17:33:59 +0100

    mapview programatically

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -7,8 +7,19 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        
+        mapView = MKMapView()
+        
+        self.view = mapView
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
