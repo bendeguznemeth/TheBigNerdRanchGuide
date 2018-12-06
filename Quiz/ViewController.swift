fda1064104ccf55edeef84a441fe2a210d013fbc
Bendegúz Németh <n.bendeguz3@gmail.com>
2018-12-06 16:51:14 +0100

    added second question label for fade-out, fade-in animation

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
     private let questions: [String] = [
@@ -29,13 +30,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = questions[currentQuestionIndex]
+        currentQuestionLabel.text = questions[currentQuestionIndex]
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        questionLabel.alpha = 0
+        nextQuestionLabel.alpha = 0
     }
 
     @IBAction func onNextQuestionClick(_ sender: UIButton) {
@@ -45,8 +46,7 @@ class ViewController: UIViewController {
         }
         
         let question: String = questions[currentQuestionIndex]
-        questionLabel.alpha = 0
-        questionLabel.text = question
+        nextQuestionLabel.text = question
         answerLabel.text = "???"
         
         animateLabelTransitions()
@@ -64,7 +64,8 @@ class ViewController: UIViewController {
 //        }
         
         UIView.animate(withDuration: 0.5, animations: {
-            self.questionLabel.alpha = 1
+            self.currentQuestionLabel.alpha = 0
+            self.nextQuestionLabel.alpha = 1
         })
     }
     
