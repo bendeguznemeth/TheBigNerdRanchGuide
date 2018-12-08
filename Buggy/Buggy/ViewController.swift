e46e5d8b3de055d2a4b504847e2fa25fd90e537d
Bendegúz Németh <n.bendeguz3@gmail.com>
2018-12-08 10:32:58 +0100

    created project and tap me button

---
+++ Buggy/Buggy/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Buggy
+//
+//  Created by Németh Bendegúz on 2018. 12. 07..
+//  Copyright © 2018. Németh Bendegúz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Called buttonTapped(_:)")
+    }
+
+}
+
