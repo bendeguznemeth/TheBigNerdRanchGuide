0485afee3f6fbaaf36c8f041a5a5a2a64bc08e04
Bendegúz Németh <n.bendeguz3@gmail.com>
2018-12-10 17:11:52 +0100

    using dequeReusableCell

--- Homepwner/Homepwner/ItemsTableViewController.swift
+++ Homepwner/Homepwner/ItemsTableViewController.swift
@@ -34,7 +34,9 @@ class ItemsTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+//        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
         
         let item = itemStore.allItems[indexPath.row]
         
