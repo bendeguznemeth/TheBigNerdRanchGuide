1797464ce60b850f65c04afc98f26e690666f517
Bendegúz Németh <n.bendeguz3@gmail.com>
2018-12-13 13:18:05 +0100

    added outlets to detailviewcontroller

--- Homepwner/Homepwner/DetailViewController.swift
+++ Homepwner/Homepwner/DetailViewController.swift
@@ -10,10 +10,43 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
+    
+    var item: Item!
+    
+    let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }()
+    
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        nameField.text = item.name
+        serialNumberField.text = item.serialNumber
+//        valueField.text = "\(item.valueInDollars)"
+//        dateLabel.text = "\(item.dateCreated)"
+        valueField.text = numberFormatter.string(from: NSNumber(value: item.valueInDollars))
+        dateLabel.text = dateFormatter.string(from: item.dateCreated)
     }
     
 
