70305b604725ccda95d152ec54a4678f432ba215
Bendegúz Németh <n.bendeguz3@gmail.com>
2018-12-18 11:02:20 +0100

    application states

--- Homepwner/Homepwner/AppDelegate.swift
+++ Homepwner/Homepwner/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let itemStore = ItemStore()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print(#function)
         
         let imageStore = ImageStore()
         
@@ -29,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        print("applicationWillResignActive")
+        print(#function)
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -42,22 +43,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             print("Could not save any of the Items")
         }
         
-        print("applicationDidEnterBackground")
+        print(#function)
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        print("applicationWillEnterForeground")
+        print(#function)
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        print("applicationDidBecomeActive")
+        print(#function)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        print("applicationWillTerminate")
+        print(#function)
     }
 
 
