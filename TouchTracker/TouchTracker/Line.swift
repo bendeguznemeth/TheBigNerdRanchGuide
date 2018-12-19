9eb50aca94cca5fe694ee15083373648450fbd5c
Bendegúz Németh <n.bendeguz3@gmail.com>
2018-12-19 14:11:55 +0100

    created line and drawview

---
+++ TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Németh Bendegúz on 2018. 12. 19..
+//  Copyright © 2018. Németh Bendegúz. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
