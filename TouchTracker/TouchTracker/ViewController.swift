1363dddb2c878e7b84333a3526e1ac73e973eafe
Bendegúz Németh <n.bendeguz3@gmail.com>
2018-12-19 10:17:48 +0100

    initial commit

---
+++ TouchTracker/TouchTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchTracker
+//
+//  Created by Németh Bendegúz on 2018. 12. 19..
+//  Copyright © 2018. Németh Bendegúz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
