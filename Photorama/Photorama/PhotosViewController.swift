32e93ad3962de0fdead24111f3de57ff50f0e07c
Bendegúz Németh <n.bendeguz3@gmail.com>
2018-12-29 11:37:25 +0100

    initial UI

---
+++ Photorama/Photorama/PhotosViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Németh Bendegúz on 2018. 12. 29..
+//  Copyright © 2018. Németh Bendegúz. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
