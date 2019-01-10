ab11b0eab629875add8cb162d3a2add5bab5ed8b
Bendegúz Németh <n.bendeguz3@gmail.com>
2019-01-10 10:16:53 +0100

    add tags to photos

--- Photorama/Photorama/PhotoInfoViewController.swift
+++ Photorama/Photorama/PhotoInfoViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class PhotoInfoViewController: UIViewController {
-
+    
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var viewCountLabel: UILabel!
     
@@ -23,7 +23,7 @@ class PhotoInfoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         store.fetchImage(for: photo) { (result) -> Void in
             switch result {
             case let .success(image):
@@ -57,14 +57,19 @@ class PhotoInfoViewController: UIViewController {
         }
     }
     
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        switch segue.identifier {
+        case "showTags":
+            let navController = segue.destination as! UINavigationController
+            let tagController = navController.topViewController as! TagsTableViewController
+            
+            tagController.store = store
+            tagController.photo = photo
+        default:
+            preconditionFailure("Unexpected segue identifier.")
+        }
     }
-    */
-
+    
 }
